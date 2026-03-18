766e06f379e5b946a69ec0f004f5f3cc8e1e556a
RCHADE <rchdbdhmr@gmail.com>
2026-03-18 22:49:55 +0100

    Add .gitignore, remove generated files from tracking

--- 12_ros2_robot_investigation/build/investigation_robot/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/root/GitHub/Projets-de-mecatronique/12_ros2_robot_investigation/install/investigation_robot'
